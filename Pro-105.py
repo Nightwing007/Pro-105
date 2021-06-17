cac0f202cc0d27b41f89c02fbcd8913f9bec301b
Nightwing007 <76204788+Nightwing007@users.noreply.github.com>
2021-06-17 15:12:27 +0530

    Create Pro-105.py

---
+++ Pro-105.py
@@ -0,0 +1,34 @@
+import math
+import csv
+
+with open("data.csv",newline="") as f :
+  reader = csv.reader(f)
+  fileData = list(reader)
+
+data = fileData[0]
+
+def mean(data):
+  n = len(data)
+  total = 0
+  for x in data :
+    total += int(x)
+  mean = total/n
+  return(mean)
+
+squaredList=[]
+
+for number in data :
+  a = int(number) - mean(data)
+  a = a**2
+  squaredList.append(a) 
+
+sum = 0
+
+for i in squaredList :
+  sum += i
+
+
+n1 = sum/(len(data)-1)
+
+standardDev = math.sqrt(n1)
+print(standardDev)
